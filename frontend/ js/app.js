1a88d9c41ac40039808603fa6cee071e524c1666
b21736758-create <b21736758@gmail.com>
2025-12-14 23:09:31 +0530

    Update app.js

--- frontend/ js/app.js
+++ frontend/ js/app.js
@@ -1,1 +1,6 @@
-
+async function searchJobs() {
+  let res = await fetch("https://YOUR-RENDER-URL/jobs");
+  let data = await res.json();
+  document.getElementById("jobs").innerHTML =
+    data.map(j => `<p>${j.title} - ${j.location}</p>`).join("");
+}
