9d181834ca21b77e30eb9a9ce0f04a793bb079fb
b21736758-create <b21736758@gmail.com>
2025-12-14 23:08:09 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,29 @@
+from fastapi import FastAPI
+from database import engine, Base
+from models import User, Job
+from sqlalchemy.orm import Session
+from database import SessionLocal
 
+app = FastAPI()
+Base.metadata.create_all(bind=engine)
+
+@app.post("/signup")
+def signup(name: str, email: str, password: str, role: str):
+    db = SessionLocal()
+    user = User(name=name, email=email, password=password, role=role)
+    db.add(user)
+    db.commit()
+    return {"msg": "signup success"}
+
+@app.post("/jobs")
+def post_job(title: str, location: str, skills: str):
+    db = SessionLocal()
+    job = Job(title=title, location=location, skills=skills)
+    db.add(job)
+    db.commit()
+    return {"msg": "job posted"}
+
+@app.get("/jobs")
+def list_jobs():
+    db = SessionLocal()
+    return db.query(Job).all()
