beabf56414890c3bcd064c04903b89e20915427a
b21736758-create <b21736758@gmail.com>
2025-12-14 23:37:18 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -33,3 +33,26 @@ def home():
         "app": "Rojeasy Jobs",
         "status": "Backend running 🚀"
     }
+from fastapi import FastAPI, HTTPException
+from auth import hash_password, create_access_token
+from models import User
+from database import SessionLocal
+
+@app.post("/signup")
+def signup(full_name: str, email: str, password: str, role: str):
+    db = SessionLocal()
+
+    if db.query(User).filter(User.email == email).first():
+        raise HTTPException(status_code=400, detail="Email already exists")
+
+    user = User(
+        full_name=full_name,
+        email=email,
+        password=hash_password(password),
+        role=role
+    )
+    db.add(user)
+    db.commit()
+
+    token = create_access_token({"sub": email, "role": role})
+    return {"access_token": token, "role": role}
