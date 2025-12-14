ae2119c43deba6557fb545a30ee9de829526654c
b21736758-create <b21736758@gmail.com>
2025-12-14 23:36:50 +0530

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -16,3 +16,13 @@ class Job(Base):
     location = Column(String)
     skills = Column(String)
     employer_id = Column(Integer)
+from sqlalchemy import Column, Integer, String
+from database import Base
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    full_name = Column(String)
+    email = Column(String, unique=True)
+    password = Column(String)
+    role = Column(String)  # candidate / employer
